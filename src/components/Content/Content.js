33baf222b4a63c6c43395997d51ffa188048aa78
Pavle Janjusevic <axeofthedead@gmail.com>
2018-05-06 15:37:33 +0200

    Created structure

---
+++ src/components/Content/Content.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import Fresh from '../Fresh/Fresh'
+import LastSearched from '../LastSearched/LastSearched'
+import Reccomended from '../Reccomendations/Reccomended'
+import Soon from '../Soon/Soon'
+
+class Content extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Content;
