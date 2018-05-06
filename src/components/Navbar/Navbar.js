6d0cddd4fb818965424d4df705892e585d1fc506
Pavle Janjusevic <axeofthedead@gmail.com>
2018-05-06 15:17:03 +0200

    etup

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <div className="logo">
+        <h1>Watch it</h1>
+        <img src="http:\\via.placeholder\100x100"></img>
+        </div>
+        
+      </div>
+    )
+  }
+}
+
+export default Navbar;
